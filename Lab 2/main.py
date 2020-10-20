92891aa95f6a6dc3dab93311f3d711aab83ef84f
Kollbi <lkollbek@gmail.com>
2020-10-20 18:37:52 +0200

    Obsluga zespolonych

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -16,23 +16,33 @@ def kwadratowa(a,b,c):
     print("Rozwiązanie:")
     if(d > 0):                                          #Rozważanie przypadków
         if(a == 0 and c == 0):                          #Odpowiednie warunki, żeby np. nie dzielić przez 0
-            print("x1 = 0")
+            return "x1 = 0"
         elif(a == 0):
-            print("x1: ", liniowa(b,c))
+            print("x1: ", round(liniowa(b,c),4))
         else:
             x1 = (-1*b - math.sqrt(d))/(2*a)
             x2 = (-1*b + math.sqrt(d))/(2*a)
             print('x1:', round(x1,2), 'x2:', round(x2,2))
     elif(d == 0):
         if(a == 0 and c == 0):
-            print("x1: 0")
+            return "x1 = 0"
         elif(a == 0):
             print("x1: ", liniowa(b,c))
         else:
             x1 = float((-1*b)/(2*a))
             print("x1: ", round(x1,2))
     else:
-        print("Program nie oblicza pierwiastków w płaszczyźnie zespolonej")
+        if(a == 0 and c == 0):
+            return "x1 = 0"
+        elif(a == 0):
+            print("x1: ", liniowa(b,c))
+        else:
+            #obsługa liczb zespolonych (?)
+            dzesp1 = math.sqrt(-1*d)       #+ "i"      #dla ładnego zapisu
+            dzesp2 = -1*math.sqrt(-1*d)    #+ "i"
+            x1 = str(-1*b/2*a) + " + " + str(round(dzesp1/(2*a),3)) + "i"
+            x2 = str(-1*b/2*a) + " " + str(round(dzesp2/(2*a),3)) + "i"
+            print("x1:",x1 ,"x2:", x2)
 
 #   TEST CASES
 print(kwadratowa(1,-2,1)) # delta = 0
@@ -40,8 +50,9 @@ print(kwadratowa(5,90,5)) # delta > 0
 print(kwadratowa(-78,0,5))
 print(kwadratowa(0,51,5))
 print(kwadratowa(0,45,0))
-print(kwadratowa(1,2,10))
+print(kwadratowa(1,2,10))   # zespolone 
 print(kwadratowa(-88,-5,1))
+print(kwadratowa(1,0,50))   #zespolone
 print(kwadratowa(0,0,0))
 
 #   USER CASE
--- Tests/requestSite.py
+++ Tests/requestSite.py
@@ -1,6 +1,6 @@
 import requests
 
-for i in range(30,40):
+for i in range(10,50):
     req = requests.get("http://rekru.kn-bit-cyber.ninja/division?id="+str(i)+"")
     print (req.status_code)
     print (req.text)
