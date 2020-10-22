06ca7e798275a28181a38a5391960554fdd9dd00
Kollbi <lkollbek@gmail.com>
2020-10-22 23:10:36 +0200

    Upgrade

--- Lab 2/main.py
+++ Lab 2/main.py
@@ -1,19 +1,15 @@
 #   Import biblioteki głównie dla funkcji sqrt()
 import math
 #   Wprowadzenie danych przez użytkownika
-# x = float((input("Podaj współczynnik a: ")))
-# y = float((input("Podaj współczynnik b: ")))
-# z = float((input("Podaj współczynnik c: ")))
+x = float((input("Podaj współczynnik a: ")))
+y = float((input("Podaj współczynnik b: ")))
+z = float((input("Podaj współczynnik c: ")))
 
-def liniowa(x,y):
+def liniowa(x, y):
     return (-1*y)/x
 
-def kwadratowa(a,b,c):
+def kwadratowa(a , b , c):
     d = float(b*b - 4*a*c)                              #Liczenie delty
-    print("------------------------------")
-    print("a:", a, " b:",b, " c:",c)
-    print("Delta:",d)
-    print("Rozwiązanie:")
     if(d > 0):                                          #Rozważanie przypadków
         if(a == 0 and c == 0):                          #Odpowiednie warunki, żeby np. nie dzielić przez 0
             return "x1 = 0"
@@ -39,27 +35,24 @@ def kwadratowa(a,b,c):
         else:
             #obsługa liczb zespolonych (?)
             dzesp1 = math.sqrt(-1*d)       #+ "i"      #dla ładnego zapisu
-            dzesp2 = -1*math.sqrt(-1*d)    #+ "i"
+            dzesp2 = -1*math.sqrt(-1*d)    #+ "i"       #części urojone
             x1 = str(-1*b/2*a) + " + " + str(round(dzesp1/(2*a),3)) + "i"
             x2 = str(-1*b/2*a) + " " + str(round(dzesp2/(2*a),3)) + "i"
-            print("x1:",x1 ,"x2:", x2)
+            print("x1:", x1 ,"x2:", x2)
 
 #   TEST CASES
-print(kwadratowa(1,-2,1)) # delta = 0
-print(kwadratowa(5,90,5)) # delta > 0
-print(kwadratowa(-78,0,5))
-print(kwadratowa(0,51,5))
-print(kwadratowa(0,-45,0))
-print(kwadratowa(1,2,10))   # zespolone 
-print(kwadratowa(-88,-5,1))
-print(kwadratowa(1,0,50))   #zespolone
-print(kwadratowa(0,0,0))
+# print(kwadratowa(1,-2,1)) # delta = 0
+# print(kwadratowa(5,90,5)) # delta > 0
+# print(kwadratowa(-78.31,0,5))
+# print(kwadratowa(0,51,5))
+# print(kwadratowa(0,-45,0))4
+# print(kwadratowa(1,2,10))   # zespolone 
+# print(kwadratowa(-88,-5,1))
+# print(kwadratowa(1,0,50))   #zespolone
+# print(kwadratowa(0,0,0))
 
 #   USER CASE
-# print(kwadratowa(x,y,z))
+print(kwadratowa(x,y,z))
 
-#a = 0 -> liniowa
-#a = 0; b = 0 -> stała
-#b = 0 -> pierwiastek
-#b = 0 ; c = 0 - > 0
-#a = 0 ; c = 0 -> 0
+#Try, Catch, Expect -> upgrade in future when i get better :))
+#Also make code better to read
