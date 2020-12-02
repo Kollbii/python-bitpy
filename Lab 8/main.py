20ad722b99d36cd247cdcc35fb9525439969546e
Kollbi <lkollbek@gmail.com>
2020-12-02 17:04:36 +0100

    Srednio

--- Lab 8/main.py
+++ Lab 8/main.py
@@ -47,17 +47,6 @@ def knight_problem_solve(board):
                             return knight_problem_solve(board)
     return board
 
-_moves = [
-    [2,-1],
-    [2,1],  
-    [1,2],
-    [-1,2],
-    [-2,-1],
-    [1,-2],
-    [-1,-2],
-    [-2,1]
-]
-
 def find_next_pos(pos):
     if pos == 63:
         return [4, 3]       # Środek boardu, pozycja 0
@@ -68,7 +57,7 @@ def find_next_pos(pos):
 
 def knight_problem_solve_from_solution(board, c):
     if c == 63:
-        return "Koniec trasy!"
+        return True
 
     for i in range (0, len(board)):
         for j in range (0, len(board[i])):
@@ -77,12 +66,33 @@ def knight_problem_solve_from_solution(board, c):
                 next_pos = find_next_pos(pos)
                 board[i][j] = f'{RED}♞{RESET}'
                 board[next_pos[0]][next_pos[1]] = f'{GREEN}♞{RESET}'
+                
                 os.system('cls')
                 print(next_pos)
                 print(draw_tab(board))
                 time.sleep(0.2)
+                
                 c += 1
                 return knight_problem_solve_from_solution(board, c)
+
+def knight_problem_2nd(board, x, y, move_x, move_y, pos):
+    if pos == 64:
+        return True
+
+    for i in range (len(board)):
+        new_x = x + move_x[i]
+        new_y = y + move_y[i]
+        if (new_x >= 0 and new_x < len(board) and new_y >= 0 and new_y < len(board) and board[new_x][new_y] == '•'):
+            board[new_x][new_y] = f'{GREEN}♞{RESET}'
+            board[x][y] = f'{RED}♞{RESET}'
+            os.system('cls')
+            print(draw_tab(board))
+            time.sleep(0.1)
+            if(knight_problem_2nd(board, new_x, new_y, move_x, move_y, pos+1)):
+                return True
+
+            board[new_x][new_y] = '•'
+    return False
 
 solution = [        
     [ 5, 18, 57, 36,  3, 16, 59, 46],
@@ -95,12 +105,32 @@ solution = [
     [40, 53, 22,  9, 42, 51, 24, 11],
 ]
 
+_moves = [
+    [2,1],
+    [1,2],
+    [-1,2],
+    [-2,1],
+    [-2,-1],
+    [-1,-2],
+    [1,-2],
+    [2,-1],
+]
+
 def start():
-    for i in range(0, 1):
-        board = [['•' for i in range(8)] for j in range(8)]
-        knights_board = get_start_position_knight(board)
-        # checked_board = knight_problem_solve(knights_board)
-        knight_problem_solve_from_solution(knights_board, 0)
-        # print(draw_tab(checked_board))
+    move_x = [2, 1, -1, -2, -2, -1, 1, 2]
+    move_y = [1, 2, 2, 1, -1, -2, -2, -1]
+    board = [['•' for i in range(8)] for j in range(8)]
+    # knights_board = get_start_position_knight(board)
+    # checked_board = knight_problem_solve(knights_board)
+    # if knight_problem_solve_from_solution(knights_board, 0):
+    #     print("Koniec trasy! :)")
+    pos = 1
+    rand_x = random.randint(0,7)
+    rand_y = random.randint(0,7)
+    board[rand_x][rand_y] = 0
+    if(knight_problem_2nd(board, rand_x, rand_y, move_x, move_y, pos)):
+        print(draw_tab(board))
+    else:
+        print("Nie ma rozwiązania :(")
 
 start()
