26a81ba4853d891f1fc30e4017b14940351fc8ea
Kollbi <lkollbek@gmail.com>
2021-01-12 18:25:56 +0100

    Tests for pytest

--- Lab_04/main.py
+++ Lab_04/main.py
@@ -2,23 +2,36 @@ import re
 import random
 
 class TestTakeOut:
-    def testTakeOutString(self):
-        string1 = "0standard0"
-        result1 = main(string1)
-        assert result1 == "standard"
 
-        string2 = "przykladbezer"
-        result2 = main(string2)
-        assert result2 == "Podano jedno lub żadnych zer\n"
+    def testTakeOutString1(self):
+        string = "0standard0"
+        result = main(string)
+        assert result == "standard"
+    
+    def testTakeOutString2(self):
+        string = "przykladbezer"
+        result = main(string)
+        assert result == "Podano jedno lub żadnych zer\n"
 
-        string3 = "aaa0Siemaneczko0Duzo0Zer0aaa"
-        result3 = main(string3)
-        assert result3 == "SiemaneczkoDuzoZer"
+    def testTakeOutString3(self):
+        string = "aaa0Siemaneczko0Duzo0Zer0aaa"
+        result = main(string)
+        assert result == "SiemaneczkoDuzoZer"
 
-        string4 = "000000000434854719300"
-        result4 = main(string4)
-        assert result4 == "4348547193"
+    def testTakeOutString4(self):
+        string = "000000000434854719300"
+        result = main(string)
+        assert result == "4348547193"
 
+    def testTakeOutString5(self):
+        string = "0standard0"
+        result = main(string)
+        assert result == "standard"
+
+    def testTakeOutString6(self):
+        string = "Pominę0Parę000Wyrazów00000BezZeraNaKoncuIPoczątku"
+        result = main(string)
+        assert result == "ParęWyrazów"
 
 def indexesOf0(stringArr):
     indexes = []
@@ -53,7 +66,6 @@ def getDecFromString(givenString):
         decString.append(ord(stringArr[i]))
     return decString
 
-
 def main(givenString):
     a = re.search(('0(.+?)0'), givenString)
     if(a):
@@ -64,20 +76,9 @@ def main(givenString):
             return f"Jest mniej niż 5 kodów ASCII\n"
         else:
             return final
-            # return f"Podany string: \t\t\t{givenString} \nString po wycięciu z zer: \t{final} \nKody ASCII: \n{getDecFromString(final)} \n5 co do wartości kod ASCII: {sortedFinalASCII[4]}\n"
+            #return f"Podany string: \t\t\t{givenString} \nString po wycięciu z zer: \t{final} \nKody ASCII: \n{getDecFromString(final)} \n5 co do wartości kod ASCII: {sortedFinalASCII[4]}\n"
     else:
         return "Podano jedno lub żadnych zer\n"
-
-# Test Case
-# Zakładamy, że dane są poprawne i jest minimum 5 znaków
-
-# print(main("0standard0"))                                         # standard | 115
-# print(main("przykladbezer"))                                      # -
-# print(main("wiecejniek0mpletnego"))                               # -
-# print(main("aaa0Siemaneczko0Dużo0Zer0aaa"))                       # SiemaneczkoDuzoZer | 99
-# print(main("000000000434854719300"))                              # 4348547193 | 55
-# print(main("Pominę0Parę000Wyrazów00000BezZeraNaKoncuIPoczątku"))  # ParęWyrazów | 119
-# print(main("Losowy00CiągZnaków0Oddzielony0Zerami000"))            # CiągZnakówOddzielonyZerami | 100
 
 # randomowy string o długości od 10 do 50 znaków
 # ASCII od 48 do 122
@@ -96,8 +97,8 @@ def randomString():
 
     return randString
 
-print(main(randomString()))
-print(main(randomString()))
-print(main(randomString()))
-print(main(randomString()))
-print(main(randomString()))
+# print(main(randomString()))
+# print(main(randomString()))
+# print(main(randomString()))
+# print(main(randomString()))
+# print(main(randomString()))
