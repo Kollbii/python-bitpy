219b5ed20e941298afce996a40e8691cf94457a0
Kollbi <lkollbek@gmail.com>
2021-01-12 18:26:42 +0100

    Tests for unittest

---
+++ Lab_04/test_main.py
@@ -0,0 +1,20 @@
+import unittest
+import main
+
+class TestMyFile(unittest.TestCase):
+    
+    def test_main(self):
+        result = main.main("0standard0")
+        self.assertEqual(result, "standard")
+    
+    def test_main1(self):
+        result = main.main("000000000434854719300")
+        self.assertEqual(result, "4348547193")
+
+    def test_main2(self):
+        result = main.main("przykladbezer")
+        self.assertEqual(result, "Podano jedno lub żadnych zer\n")
+
+    def test_main3(self):
+        result = main.main("Pominę0Parę000Wyrazów00000BezZeraNaKoncuIPoczątku")
+        self.assertEqual(result, "ParęWyrazów")
